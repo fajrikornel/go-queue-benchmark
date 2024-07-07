dbb4eb8b11c948bdc2d7e3ecf689dd06daab1f73
Fajri Kornel <fajri.apk@gmail.com>
2024-07-07 16:21:51 +0700

    add test for enqueue on empty queue

--- internal/queue/queue_test.go
+++ internal/queue/queue_test.go
@@ -1,6 +1,8 @@
 package queue
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestEnqueue(t *testing.T) {
 	testCases := []struct {
@@ -9,6 +11,20 @@ func TestEnqueue(t *testing.T) {
 	}{
 		{
 			"test_enqueue_on_empty_queue", func(t *testing.T) {
+				queue := New[int](1)
+
+				val := 123
+				queue.Enqueue(val)
+
+				if queue.front != 0 {
+					t.Fatalf("Queue front not 1")
+				}
+				if queue.back != 0 {
+					t.Fatalf("Queue back not 1")
+				}
+				if queue.queue[0] != val {
+					t.Fatalf("Queue value not %d", val)
+				}
 			},
 		},
 	}
